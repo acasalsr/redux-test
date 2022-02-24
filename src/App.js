fffa6de8ad8a3cf9e40d6ee88e266d91d67db32c
andreu casals <acasalsr@gmail.com>
2022-02-24 17:33:21 +0100

    exercici counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  let counter = (state) => state + 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(counter)}>+1</button>
+    </>
   );
 }
 
 export default App;
+
+
+
